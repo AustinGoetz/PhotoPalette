6769ee2dc86d9c0a6b57b99038d0d1b10e465ed1
Austin Goetz <a.goetz09@gmail.com>
2019-10-22 11:15:11 -0600

    constrained views, added API helper functions, built out properties list

--- PaletteiOS29/ViewControllers/PaletteListViewController.swift
+++ PaletteiOS29/ViewControllers/PaletteListViewController.swift
@@ -13,9 +13,32 @@
 import UIKit
 
 class PaletteListViewController: UIViewController {
+    
+    // MARK: - Properties
+    var safeArea: UILayoutGuide {
+        return self.view.safeAreaLayoutGuide
+    }
+    
+    var buttons: [UIButton] {
+        return[featuredButton, doubleRainbowButton, randomButton]
+    }
+    
+    var photos: [UnsplashPhoto] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - API Helper Function
+    
+    func searchForCategory(_ route: UnsplashRoute) {
+        UnsplashService.shared.fetchFromUnsplash(for: route) { (photos) in
+            DispatchQueue.main.async {
+                guard let photos = photos else { return }
+                self.photos = photos
+                self.paletteTableView.reloadData()
+            }
+        }
     }
 
     // MARK: - Create SubViews (Part One)
@@ -85,7 +108,43 @@ class PaletteListViewController: UIViewController {
         paletteTableView.register(PaletteTableViewCell.self, forCellReuseIdentifier: "paletteCell")
         paletteTableView.allowsSelection = false
     }
+    
+    // MARK: - Constrain Views (Part Three)
+    
+    func constrainViews() {
+        paletteTableView.anchor(top: buttonStackView.bottomAnchor, bottom: self.safeArea.bottomAnchor, leading: self.safeArea.leadingAnchor, trailing: self.safeArea.trailingAnchor, topPadding: (buttonStackView.frame.height/2), bottomPadding: 0, leadingPadding: 0, trailingPadding: 0)
+        
+        buttonStackView.anchor(top: self.safeArea.topAnchor, bottom: nil, leading: self.safeArea.leadingAnchor, trailing: self.safeArea.trailingAnchor, topPadding: 0, bottomPadding: 0, leadingPadding: 8, trailingPadding: 8)
+    }
+    
+    // MARK: - IBAction For Buttons
+    func activateButtons() {
+        buttons.forEach{ $0.addTarget(self, action: #selector(searchButtonTapped(sender:)), for: .touchUpInside)}
+    }
+    
+    @objc func searchButtonTapped(sender: UIButton) {
+        selectButton(sender)
+        switch sender {
+        case randomButton:
+            searchForCategory(.random)
+        case doubleRainbowButton:
+            searchForCategory(.doubleRainbow)
+        case featuredButton:
+            searchForCategory(.featured)
+        default:
+            print("Error, button was not found")
+        }
+    }
+    
+    func selectButton(_ button: UIButton) {
+        
+        buttons.forEach {$0.setTitleColor(UIColor(named: "offWhite"), for: .normal)}
+        button.setTitleColor(UIColor(named: "devMountainBlue"), for: .normal)
+    }
+    
+    
 }
+
 
 extension PaletteListViewController: UITableViewDelegate, UITableViewDataSource {
     
