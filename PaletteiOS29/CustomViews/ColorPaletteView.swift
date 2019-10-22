3d51c35ffaca6cbde59c6ab78f5541634cf4b1e5
Austin Goetz <a.goetz09@gmail.com>
2019-10-22 12:06:22 -0600

    added color palette view and view cell functionality

--- PaletteiOS29/CustomViews/ColorPaletteView.swift
+++ PaletteiOS29/CustomViews/ColorPaletteView.swift
@@ -24,4 +24,22 @@ class ColorPaletteView: UIView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    // MARK: - Create SubViews (Step One)
+    
+    lazy var colorStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        
+        return stackView
+    }()
+    
+    // MARK: - Add SubViews and Constrain (Steps 2 and 3)
+    
+    private func setUpViews() {
+        addSubview(colorStackView)
+        colorStackView.anchor(top: topAnchor, bottom: bottomAnchor, leading: leadingAnchor, trailing: trailingAnchor, topPadding: 0, bottomPadding: 0, leadingPadding: 0, trailingPadding: 0)
+    }
 }
--- PaletteiOS29/CustomViews/PaletteTableViewCell.swift
+++ PaletteiOS29/CustomViews/PaletteTableViewCell.swift
@@ -9,15 +9,17 @@
 import UIKit
 
 class PaletteTableViewCell: UITableViewCell {
-
+    
     var photo: UnsplashPhoto? {
         didSet {
-            // Update Views
+            updateViews()
+            
         }
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        setUpViews()
     }
     
     // MARK: - Declare SubViews (Step One)
@@ -25,7 +27,7 @@ class PaletteTableViewCell: UITableViewCell {
     lazy var paletteImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.layer.masksToBounds = true
-        imageView.layer.cornerRadius = (contentView.frame.height/10)
+        imageView.layer.cornerRadius = (contentView.frame.height / 10)
         imageView.contentMode = .scaleAspectFill
         
         return imageView
@@ -38,8 +40,59 @@ class PaletteTableViewCell: UITableViewCell {
     }()
     
     lazy var colorPaletteView: ColorPaletteView = {
-       let paletteView = ColorPaletteView()
+        let paletteView = ColorPaletteView()
         
         return paletteView
     }()
+    
+    // MARK: - Add SubViews to Cell (Step Two)
+    
+    func addAllSubviews() {
+        self.addSubview(paletteImageView)
+        self.addSubview(paletteImageView)
+        self.addSubview(colorPaletteView)
+    }
+    
+    // MARK: - Constrain Views (Step Three)
+    
+    func setUpViews() {
+        addAllSubviews()
+        
+        let imageDimensions = (contentView.frame.width - (SpacingConstants.outerHorizontalPadding * 2))
+        
+        paletteImageView.anchor(top: contentView.topAnchor, bottom: nil, leading: contentView.leadingAnchor, trailing: contentView.trailingAnchor, topPadding: SpacingConstants.outerVerticalPadding, bottomPadding: 0, leadingPadding: SpacingConstants.outerHorizontalPadding, trailingPadding: SpacingConstants.outerHorizontalPadding, width: imageDimensions, height: imageDimensions)
+        
+        paletteTitleLabel.anchor(top: paletteImageView.bottomAnchor, bottom: nil, leading: contentView.leadingAnchor, trailing: contentView.trailingAnchor, topPadding: SpacingConstants.verticalObjectBuffer, bottomPadding: 0, leadingPadding: SpacingConstants.outerHorizontalPadding, trailingPadding: SpacingConstants.outerHorizontalPadding, width: nil, height: SpacingConstants.oneLineElementHeight)
+        
+        colorPaletteView.anchor(top: paletteTitleLabel.bottomAnchor, bottom: contentView.bottomAnchor, leading: contentView.leadingAnchor, trailing: contentView.trailingAnchor, topPadding: SpacingConstants.verticalObjectBuffer, bottomPadding: SpacingConstants.outerVerticalPadding, leadingPadding: SpacingConstants.outerHorizontalPadding, trailingPadding: SpacingConstants.outerHorizontalPadding, width: nil, height: SpacingConstants.twoLineElementHeight)
+        
+        colorPaletteView.clipsToBounds = true
+        colorPaletteView.layer.cornerRadius = SpacingConstants.twoLineElementHeight / 2
+    }
+    
+    func updateViews() {
+        guard let photo = photo else { return }
+        
+        fetchAndSetImage(for: photo)
+        fetchAndSetColors(for: photo)
+        
+        paletteTitleLabel.text = photo.description
+    }
+    
+    func fetchAndSetImage(for photo: UnsplashPhoto) {
+        UnsplashService.shared.fetchImage(for: photo) { (image) in
+            DispatchQueue.main.async {
+                self.paletteImageView.image = image
+            }
+        }
+    }
+    
+    func fetchAndSetColors(for photo: UnsplashPhoto) {
+        ImaggaService.shared.fetchColorsFor(imagePath: photo.urls.regular) { (colors) in
+            DispatchQueue.main.async {
+                guard let colors = colors else { return }
+                self.colorPaletteView.colors = colors
+            }
+        }
+    }
 }
